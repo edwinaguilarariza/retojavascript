04964271ad4a40e1efb61573fd1c8c6fab3b87d1
edwinaguilarariza <edwinaguilarariza1@yahoo.com>
2021-03-03 19:38:40 -0500

    reto java script

---
+++ ensayo.js
@@ -0,0 +1,124 @@
+
+
+
+const animales= () => {
+    
+    let op = prompt(
+        "seleccione uno de los siguentes tipos de animales:  \n 1 - vertebrados \n 2 - invertebrados \n 3 - hervivoros \n 4 - carnivoros ");
+
+       
+        if (op !== "1" && op !== "2" && op !== "3" && op !== "4") {
+            alert("seleccione una de las opciones");
+        } else { 
+
+            if (op !== "1") {
+                
+            } else {
+                let op = prompt(
+                    "seleccione uno de los siguentes animales vertebrados:  \n a - ballenas \n b - delfines \n c - perros \n d - gatos ");
+                    if (op === "a") {
+                        alert("las ballenas son grandes");
+                    }
+                    if (op === "b") {
+                        alert("las delfines son acuaticos");
+                    }
+                    if (op === "c") {
+                        alert("las perros son el mejor amigo del hombre");
+                    }
+                    if (op === "d") {
+                        alert("los gatos son felinos");
+                    };
+                    
+            };
+            
+            
+
+                if (op !== "2" ) {
+                
+                } else {
+                    
+                    let op = prompt(
+                        "seleccione uno de los siguentes animales invertebrados:  \n e - orugas \n f - escorpiones \n g - mariposa \n h - caracoles ");
+                        if (op === "e") {
+                            alert("las orugas son misteriosas");
+                        }
+                        if (op === "f") {
+                            alert("los escorpiones son peligrosos");
+                        }
+                        if (op === "g") {
+                            alert("las mariposa son espectaculares");
+                        }
+                        if (op === "h") {
+                            alert("los caracoles curiosos");
+                        }
+                };  
+    
+    
+                if (op !== "3" ) {
+                
+                } else {
+                    
+                    let op = prompt(
+                        "seleccione uno de los siguentes animales hervivoros:  \n i - pollos \n j - pavos \n k - gallinas \n l - patos ");
+                        if (op === "i") {
+                            alert("las pollos son aliemento");
+                        }
+                        if (op === "j") {
+                            alert("las pavos son deliciosos");
+                        }
+                        if (op === "k") {
+                            alert("las gallinas ponen huevos");
+                        }
+                        if (op === "l") {
+                            alert("los patos nadan en los rios");
+                        }
+                };  
+                
+    
+                if (op !== "4" ) {
+                    
+                } else {
+                    
+                    let op = prompt(
+                        "seleccione uno de los siguentes animales carnivoros:  \n r - leones \n s - cocodrilos \n t - hombre\n v - tigres");
+                        if (op === "r") {
+                            alert("los leones son los reyes de la selva");
+                        }
+                        if (op === "s") {
+                            alert("las cocodrilos son acuaticos");
+                        }
+                        if (op === "t") {
+                            alert("el hombre evolucion de la creacion ");
+                        }
+                        if (op === "v") {
+                            alert("los tigres  son cazadores solitarios");
+                        }
+                }; 
+                
+                
+        };  
+                  
+                
+}        
+        
+        
+           
+            
+ 
+                
+           
+
+
+            
+            
+
+            
+
+     
+                
+    
+           
+         
+           
+
+    
